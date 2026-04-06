751447f68ea463ea205aa968feb5afee12f081ce
lucas_v_pedroso <lucas_v_pedroso@estudante.sesisenai.org.br>
2026-04-06 12:56:29 -0300

    add - adicionei checkbox

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.getElementById("formAdocao").addEventListener("submit", function (e){
     let moradia = document.getElementById("moradia").value;
     let quintal = document.querySelector('input[name="quintal"]:checked');
     let tempo = document.getElementById("tempo").value;
+    let motivo = document.getElementById("motivo").value;
 
     if(nome.length < 3) return alert("Nome Inválido");
     if(idade < 18) return alert("Você ainda não pode adotar um cachorro");
