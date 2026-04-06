6857c71e612ce4394c9c7f4b6b007a7e71ad303a
lucas_v_pedroso <lucas_v_pedroso@estudante.sesisenai.org.br>
2026-04-06 12:45:04 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("formAdocao").addEventListener("submit", function (e){
+    e.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    let cpf = document.getElementById("cpf").value;
+    let email = document.getElementById("email").value;
+    let moradia = document.getElementById("moradia").value;
+    let quintal = document.querySelector('input[name="quintal"]:checked');
+    let tempo = document.getElementById("tempo").value;
+
+    if(nome.length < 3) return alert("Nome Inválido");
+    if(idade < 18) return alert("Você ainda não pode adotar um cachorro");
+
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
+});
